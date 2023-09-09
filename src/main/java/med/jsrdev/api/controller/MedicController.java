f11dd883c0fd6d72650818c99e2b5ff87793e47f
Jose SR <josesanchez09190@gmail.com>
2023-09-08 22:12:45 -0600

    get medic list from MedicalDataList

--- src/main/java/med/jsrdev/api/controller/MedicController.java
+++ src/main/java/med/jsrdev/api/controller/MedicController.java
@@ -3,21 +3,31 @@ package med.jsrdev.api.controller;
 import jakarta.validation.Valid;
 import med.jsrdev.api.medic.Medic;
 import med.jsrdev.api.medic.MedicRepository;
+import med.jsrdev.api.medic.MedicalDataList;
 import med.jsrdev.api.medic.MedicalRegistrationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medics")
 public class MedicController {
 
-    @Autowired //No es recomendable usarlo para fines de testing y sirve para
-    // inyeccion automatica de una dependencia, de lo contrario hay que crear el objeto
+    @Autowired
     private MedicRepository medicRepository;
 
-    //registrar medico
+    //register medic
     @PostMapping
     public void registerPhysician(@RequestBody @Valid MedicalRegistrationData medicalRegistrationData) {
         medicRepository.save(new Medic(medicalRegistrationData));
     }
+
+    //medic list
+    @GetMapping
+    public List<MedicalDataList> medicList() {
+        return medicRepository.findAll().stream().map(MedicalDataList::new).toList();
+    }
+
+    // get medic by id
 }
--- src/main/java/med/jsrdev/api/controller/UserController.java
+++ src/main/java/med/jsrdev/api/controller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
     }
 
     @GetMapping
-    public String helloTwo() {
-        return "hello Request User";
+    public List<User> userList() {
+        return userRepository.findAll();
     }
 }
